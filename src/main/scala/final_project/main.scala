aea23d030e378845470e5785e70169f7aacf3692
Ian Brown <iannbrown12@gmail.com>
2021-05-03 19:11:54 -0400

    start

---
+++ src/main/scala/final_project/main.scala
@@ -0,0 +1,60 @@
+package project_3
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx._
+import org.apache.spark.storage.StorageLevel
+import org.apache.log4j.{Level, Logger}
+
+
+object main{
+  val rootLogger = Logger.getRootLogger()
+  rootLogger.setLevel(Level.ERROR)
+
+  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+  Logger.getLogger("org.spark-project").setLevel(Level.WARN)
+
+
+
+def alg_implement //
+
+
+
+
+
+
+
+
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setAppName("project_3")
+    val sc = new SparkContext(conf)
+    val spark = SparkSession.builder.config(conf).getOrCreate()
+
+    if(args.length!=2){
+      println("Usage: final_project graph_path output_path")
+      sys.exit(1)
+    }
+
+    if(args.length==2){
+      val startTimeMillis = System.currentTimeMillis()
+      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
+      val g = Graph.fromEdges[Int, Int](edges, 0, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
+      val g2 = LubyMIS(g) //algorithm we choose
+
+
+      val endTimeMillis = System.currentTimeMillis()
+      val durationSeconds = (endTimeMillis - startTimeMillis) / 1000
+      println("==================================")
+      println("Luby's algorithm completed in " + durationSeconds + "s.")//change this
+      println("==================================")
+
+      val g2df = spark.createDataFrame(g2.vertices)
+      g2df.coalesce(1).write.format("csv").mode("overwrite").save(args(2))
+    }
+  }
+}
